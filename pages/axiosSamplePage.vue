afd65ac459c7826c4950d9a04e4e1d7baf03265b
mintsu <mintsu>
2019-07-27 16:46:48 +0900

    add axios sample

---
+++ pages/axiosSamplePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>time:{{ data.time.updated }}</p>
+    <p>chartName:{{ data.chartName }}</p>
+    <p>USD:{{ data.bpi.USD.rate }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const response = await $axios.$get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    return {
+      data: response
+    }
+  }
+}
+</script>
